d55b208a01c27265fe3ed4854436fe13b456562e
Jordan Mele <jordan.mele@outlook.com.au>
2026-07-25 05:42:45 +0000

    Add renovate config and bulk dependencies update

---
+++ .github/workflows/update_bazel_nodejs.mjs
@@ -0,0 +1,52 @@
+import fs from "node:fs/promises";
+
+const mapping = {
+    "darwin_arm64": "darwin-arm64.tar.xz",
+    "darwin_amd64": "darwin-x64.tar.xz",
+    "linux_arm64": "linux-arm64.tar.xz",
+    "linux_ppc64le": "linux-ppc64le.tar.xz",
+    "linux_s390x": "linux-s390x.tar.xz",
+    "linux_amd64": "linux-x64.tar.xz",
+    "windows_amd64": "win-x64.zip",
+};
+
+const installShPathArg = process.argv[2];
+const moduleBazelArg = process.argv[3];
+const installShContent = await fs.readFile(installShPathArg, "utf-8");
+const nodeVersionMatch = installShContent.match(/^NODE_VERSION="v(.+)"$/m);
+if (!nodeVersionMatch) {
+    throw new Error("Could not find NODE_VERSION in install.sh");
+}
+const nodejsVersion = nodeVersionMatch[1];
+
+const res = await fetch(`https://nodejs.org/dist/v${nodejsVersion}/SHASUMS256.txt`);
+const text = await res.text();
+const lines = text.split("\n").filter(Boolean);
+const artifacts = new Map(
+    lines.map(line => {
+        const [sha, filename] = line.split(/\s+/);
+        return [filename, sha];
+    })
+);
+
+const processed = new Map();
+for (const k in mapping) {
+    const v = mapping[k];
+    const key = `${nodejsVersion}-${k}`;
+    const artifactName = `node-v${nodejsVersion}-${v}`;
+    const artifactDir = artifactName.replace(/\.tar\.xz$/, "").replace(/\.zip$/, "");
+    const artifactSha = artifacts.get(artifactName);
+    processed.set(key, [artifactName, artifactDir, artifactSha]);
+}
+
+const moduleBazelContent = await fs.readFile(moduleBazelArg, "utf-8");
+const updatedModuleBazelContent = moduleBazelContent.replace(/node\.toolchain\(.*node_version\s=\s"\d+\.\d+\.\d+",?\n?\)/s, `\
+node.toolchain(
+    node_repositories = {
+        ${Array.from(processed.entries())
+            .map(([key, [artifactName, artifactDir, artifactSha]]) => `"${key}": ("${artifactName}", "${artifactDir}", "${artifactSha}"),`)
+            .join("\n        ")}
+    },
+    node_version = "${nodejsVersion}",
+)`);
+await fs.writeFile(moduleBazelArg, updatedModuleBazelContent, "utf-8");
